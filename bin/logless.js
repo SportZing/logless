2f22968afc89844a7bf6a2fd840bd7d9e4cba740
James Brumond <kbjr14@gmail.com>
2012-04-29 18:32:27 -0700

    working on cli

---
+++ bin/logless.js
@@ -0,0 +1,25 @@
+#!/usr/bin/env node
+
+/**
+ * CLI For logless
+ */
+
+var args = process.argv.slice(2);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+/* End of file logless.js */
+/* Location: ./bin/logless.js */
