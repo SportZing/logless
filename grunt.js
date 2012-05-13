183ad299c6bebc2ca111a1f227e42e035c3259ae
James Brumond <kbjr14@gmail.com>
2012-05-12 23:24:49 -0700

    reorganized grunt.js

--- grunt.js
+++ grunt.js
@@ -12,38 +12,40 @@
  *      src: ["file1.js", "file2.js", "other/stuff/*.js"],
  *      dest: "path/to/destination",
  *      names: ["console", "alert"],
- *			strip: "path/to/remove",
+ *      strip: "path/to/remove",
  *      options: {beautify: true}
  *    }
  *  }
  *
  */
 
-var path = require('path');
-var logless = require('logless');
+var path     = require('path');
+var logless  = require('logless');
+
+var stripRegex = /[\-\[\]{}()*+?.,\\\^$|#\s]/g;
 
 module.exports = function(grunt) {
 	grunt.registerMultiTask('logless',
 		'Parses JavaScript files and removes logging statements', function() {
 			var data = this.data;
-	    var files = grunt.file.expandFiles( data.src );
-	    var strip = this.data.strip;
+			var files = grunt.file.expandFiles( data.src );
+			var strip = this.data.strip;
 
-	    if ( typeof strip === "string" ) {
-	      strip = new RegExp( "^" + grunt.template.process( strip, grunt.config() ).replace( /[\-\[\]{}()*+?.,\\\^$|#\s]/g, "\\$&" ) );
-	    }
+			if ( typeof strip === "string" ) {
+				strip = new RegExp('^' + grunt.template.process(strip, grunt.config()).replace(stripRegex, '\\$&'));
+			}
 
 			files.forEach(function(src) {
 				try {
 					var parsed = logless.parse(grunt.file.read(src), data.names, data.options);
-		      var target = path.join(data.dest,strip ? src.replace( strip, "" ) : src);
+					var target = path.join(data.dest, strip ? src.replace(strip, '') : src);
 					grunt.file.write(target, parsed);
 					grunt.log.writeln('File "' + src + '" parsed -> "' + target + '"');
 				} catch (err) {
 					grunt.log.error(src + '\n' + err.stack);
 				}
 			});
-	    grunt.log.writeln( "Processed " + files.length + " files." );
+			grunt.log.writeln('Processed ' + files.length + ' files.');
 
 			if (this.errorCount) {return false;}
 		}
